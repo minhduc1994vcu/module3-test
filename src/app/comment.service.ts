b0d2826571b3fe0bd8b8ffae60422c54edea194f
minhduc1994vcu@gmail.com <nguoitrongtiemthuc123>
2019-07-14 21:18:16 +0700

    function getCommentByPostId

---
+++ src/app/comment.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Comment} from './comment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+  private readonly API_URL = 'http://jsonplaceholder.typicode.com/comments';
+
+
+  constructor(private http: HttpClient) {
+  }
+
+  getCommentsByPostId(id: number): Observable<Comment> {
+    return this.http.get<Comment>(`${this.API_URL}?postId=${id}`);
+  }
+}
