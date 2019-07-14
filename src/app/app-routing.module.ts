c34f9b18a226a4048e6c45bc1a53eeff3318b744
minhduc1994vcu@gmail.com <nguoitrongtiemthuc123>
2019-07-14 21:40:11 +0700

    path to component comment detail

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
 import { BlogDetailComponent } from './blog-detail/blog-detail.component';
 import { BlogEditComponent } from './blog-edit/blog-edit.component';
+import {CommentDetailComponent} from './comment-detail/comment-detail.component';
 
 const routes: Routes = [{
   path: 'blog',
@@ -13,7 +14,12 @@ const routes: Routes = [{
 }, {
   path: 'blog/:id/edit',
   component: BlogEditComponent
-}];
+},
+  {
+    path: 'comment/:id',
+    component: CommentDetailComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
