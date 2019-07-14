2bcb264d93bc9235006fa53d0eed3de026c35b9d
minhduc1994vcu@gmail.com <nguoitrongtiemthuc123>
2019-07-14 22:36:44 +0700

    logic for create comment

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -4,6 +4,7 @@ import {PostService} from '../post.service';
 import {IPost} from '../post';
 import {Comment} from '../comment';
 import {CommentService} from '../comment.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-blog-detail',
@@ -13,29 +14,58 @@ import {CommentService} from '../comment.service';
 export class BlogDetailComponent implements OnInit {
   post: IPost;
   comments: Comment[];
+  comment: Comment;
 
+  commentForm: FormGroup;
   constructor(
     private route: ActivatedRoute,
     private postService: PostService,
-    private commentService: CommentService
+    private commentService: CommentService,
+    private fb: FormBuilder,
+
   ) {
   }
 
   ngOnInit() {
+    this.commentForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      body: ['', [Validators.required, Validators.minLength(10)]],
+    });
     const id = +this.route.snapshot.paramMap.get('id');
     this.postService.getPostById(id).subscribe(
       next => {
         this.post = next;
-        this.commentService.getCommentsByPostId(id).subscribe(comments => {
-            // @ts-ignore
-            this.comments = comments;
-          },
-          error => {
-            console.log(error);
-            this.post = null;
-          }
-        );
-      });
+      },
+      error => {
+        console.log(error);
+        this.post = null;
+      }
+    );
+    // @ts-ignore
+    this.commentService.getAllComment().subscribe(data => {
+      this.comments = data;
+    });
+  }
+  onSubmit() {
+    if (this.commentForm.valid) {
+      const {value} = this.commentForm;
+      const data = {
+        ...this.comment,
+        ...value
+      };
+      this.commentService.createComment(data)
+        .subscribe(next => {
+          console.log('create Comment success');
+          console.log(next);
+          this.comments.unshift(next);
+          this.commentForm.reset({
+            name: '',
+            email: '',
+            body: ''
+          });
+        }, error => console.log(error));
+    }
   }
   deleteComment(i) {
     const comment = this.comments[i];
