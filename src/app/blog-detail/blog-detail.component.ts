2bb22cacb2c479cacb140961132e978e522b4dc9
minhduc1994vcu@gmail.com <nguoitrongtiemthuc123>
2019-07-14 21:26:25 +0700

    logic for function getCommentsByPostId

--- src/app/blog-detail/blog-detail.component.ts
+++ src/app/blog-detail/blog-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PostService } from '../post.service';
-import { IPost } from '../post';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {PostService} from '../post.service';
+import {IPost} from '../post';
+import {Comment} from '../comment';
+import {CommentService} from '../comment.service';
 
 @Component({
   selector: 'app-blog-detail',
@@ -10,22 +12,29 @@ import { IPost } from '../post';
 })
 export class BlogDetailComponent implements OnInit {
   post: IPost;
-  comments: any[];
+  comments: Comment[];
+
   constructor(
     private route: ActivatedRoute,
-    private postService: PostService
-  ) {}
+    private postService: PostService,
+    private commentService: CommentService
+  ) {
+  }
 
   ngOnInit() {
     const id = +this.route.snapshot.paramMap.get('id');
     this.postService.getPostById(id).subscribe(
       next => {
         this.post = next;
-      },
-      error => {
-        console.log(error);
-        this.post = null;
-      }
-    );
+        this.commentService.getCommentsByPostId(id).subscribe(comments => {
+            // @ts-ignore
+            this.comments = comments;
+          },
+          error => {
+            console.log(error);
+            this.post = null;
+          }
+        );
+      });
   }
 }
