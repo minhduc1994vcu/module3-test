e1ac362494dee46c82b7ef3559c2786efbb39c16
minhduc1994vcu@gmail.com <nguoitrongtiemthuc123>
2019-07-14 22:56:00 +0700

    logic for comment-edit component

---
+++ src/app/comment-edit/comment-edit.component.ts
@@ -0,0 +1,62 @@
+import {Component, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {ActivatedRoute, Router} from '@angular/router';
+import {PostService} from '../post.service';
+import {CommentService} from '../comment.service';
+import {Comment} from '../comment';
+
+@Component({
+  selector: 'app-comment-edit',
+  templateUrl: './comment-edit.component.html',
+  styleUrls: ['./comment-edit.component.scss']
+})
+export class CommentEditComponent implements OnInit {
+  comment: Comment;
+  postForm: FormGroup;
+  constructor(
+    private route: ActivatedRoute,
+    private postService: PostService,
+    private commentService: CommentService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit() {
+    this.postForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      body: ['', [Validators.required, Validators.minLength(10)]],
+    });
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.commentService.getCommentById(id).subscribe(
+      next => {
+        this.comment = next;
+        this.postForm.patchValue(this.comment);
+      },
+      error => {
+        console.log(error);
+        this.comment = null;
+      }
+    );
+  }
+  onSubmit() {
+    if (this.postForm.valid) {
+      const { value } = this.postForm;
+      const data = {
+        ...this.comment,
+        ...value
+      };
+      this.commentService.updateComment(data).subscribe(
+        next => {
+          console.log('edit comment success');
+          console.log(next);
+          this.router.navigate(['/blog']);
+        },
+        error => console.log(error)
+      );
+    }
+  }
+
+
+}
